1c19119f508bce9a7fcb4018b6fe633548f26d74
Lukasz Kielkowski <lukaszkielkowski@gmail.com>
2022-07-03 12:35:29 +0200

    change from for to forEach

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ fetch("./data.json")
   });
 
 const getAmount = (amountData) => {
-  const toolTip = [...document.querySelectorAll(".tooltip")];
-  const tdStyle = [...document.querySelectorAll("td")];
+  const toolTips = [...document.querySelectorAll(".tooltip")];
+  const tdStyles = [...document.querySelectorAll("td")];
   const summary = document.querySelector(".summary__item--second");
-  let summaryamount = 0;
-  for (let i = 0; i < toolTip.length; i++) {
-    toolTip[i].textContent = amountData[i].amount;
-    console.log(amountData[i]);
+  let summaryAmount = 0;
+
+  // for (let i = 0; i < toolTip.length; i++) {
+  //   toolTip[i].textContent = amountData[i].amount;
+  //   console.log(amountData[i]);
+  //   let columntValue = amountData[i].amount / 100;
+  //   tdStyle[i].style = `--size: ${columntValue}`;
+  //   summaryamount = summaryamount + amountData[i].amount;
+  // }
+
+  toolTips.forEach((toolTip, i) => {
+    toolTip.textContent = amountData[i].amount;
     let columntValue = amountData[i].amount / 100;
-    tdStyle[i].style = `--size: ${columntValue}`;
-    summaryamount = summaryamount + amountData[i].amount;
-  }
-  summary.textContent = summaryamount;
+    tdStyles[i].style = `--size: ${columntValue}`;
+    summaryAmount = summaryAmount + amountData[i].amount;
+  });
+
+  summary.textContent = summaryAmount;
 };
 
 getAmount();
