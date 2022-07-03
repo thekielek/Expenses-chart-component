b9acaecee4f79fb6a38756123847466fc844961b
Lukasz Kielkowski <lukaszkielkowski@gmail.com>
2022-07-03 12:21:32 +0200

    create fetch in global scope

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const getAmount = () => {
-  fetch("./data.json")
-    .then((data) => data.json())
-    .then((data) => {
-      console.log(data);
-      const toolTip = [...document.querySelectorAll(".tooltip")];
-      const tdStyle = [...document.querySelectorAll("td")];
-      const summary = document.querySelector(".summary__item--second");
-      let summaryamount = 0;
-      for (let i = 0; i < toolTip.length; i++) {
-        toolTip[i].textContent = data[i].amount;
-        let columntValue = data[i].amount / 100;
-        tdStyle[i].style = `--size: ${columntValue}`;
-        summaryamount = summaryamount + data[i].amount;
-      }
-      summary.textContent = summaryamount;
-    });
+fetch("./data.json")
+  .then((data) => data.json())
+  .then((data) => {
+    const amountReturn = getAmount(data);
+  });
+
+const getAmount = (amountData) => {
+  const toolTip = [...document.querySelectorAll(".tooltip")];
+  const tdStyle = [...document.querySelectorAll("td")];
+  const summary = document.querySelector(".summary__item--second");
+  let summaryamount = 0;
+  for (let i = 0; i < toolTip.length; i++) {
+    toolTip[i].textContent = amountData[i].amount;
+    console.log(amountData[i]);
+    let columntValue = amountData[i].amount / 100;
+    tdStyle[i].style = `--size: ${columntValue}`;
+    summaryamount = summaryamount + amountData[i].amount;
+  }
+  summary.textContent = summaryamount;
 };
 
 getAmount();
